542fcab96556e3024aa58c732df8d7d35fb1ca6e
munhoz2k <lucasmunhozarruda@gmail.com>
2024-04-03 22:16:18 -0300

    feat: new section (about me)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import HeroSection from '@/app/components/HeroSection'
 import NavBar from '@/app/components/NavBar'
+import AboutSection from './components/AboutSection'
 
 export default function Home() {
   return (
-    <main className="flex flex-col h-screen">
+    <main className="flex flex-col">
       <NavBar />
 
-      <div className="container h-full px-12 py-4 mt-24 mx-auto">
+      <div className="container px-12 py-4 mt-24 mx-auto">
         <HeroSection />
+        <AboutSection />
       </div>
     </main>
   )
