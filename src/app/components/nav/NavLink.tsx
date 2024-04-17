f99bc9763e87d1c793d50edb2ec82f410ac95c81
munhoz2k <lucasmunhozarruda@gmail.com>
2024-04-16 23:13:51 -0300

    styles: underline hover effect

--- src/app/components/nav/NavLink.tsx
+++ src/app/components/nav/NavLink.tsx
@@ -9,7 +9,7 @@ export default function NavLink({ href, title }: NavLinkProps) {
   return (
     <Link
       href={href}
-      className='p-2 text-lg font-normal text-light-shades
+      className='relative underline-hover p-2 text-lg font-normal text-light-shades
       hover:text-brand-color md:p-0 md:text-xl'
     >
       {title}
