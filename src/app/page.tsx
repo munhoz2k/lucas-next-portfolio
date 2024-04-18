bd339b3d2008f4a2fcd27301e21d9f3b8d02a6f5
munhoz2k <lucasmunhozarruda@gmail.com>
2024-04-17 22:54:23 -0300

    feat: toast notification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Toaster } from 'sonner'
+
 import HeroSection from '@/app/components/hero/HeroSection'
 import NavBar from '@/app/components/nav/NavBar'
 import AboutSection from './components/about/AboutSection'
@@ -9,6 +11,16 @@ import Footer from './components/footer/Footer'
 export default function Home() {
   return (
     <main className="flex flex-col">
+      <Toaster
+        toastOptions={{
+          classNames: {
+            success: 'bg-brand-color border-0 text-white',
+            error: 'bg-zinc-200 border-0 text-rose-700'
+          }
+        }}
+        theme='light'
+      />
+
       <NavBar />
 
       <div className="container px-12 py-4 mt-24 mx-auto">
