5678f884b759d21c5d72873805a1012753a38209
munhoz2k <lucasmunhozarruda@gmail.com>
2024-04-18 22:51:25 -0300

    style: fix responsivity

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -7,24 +7,27 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='footer bg-dark-shades'>
-      <div className='container flex justify-between items-center p-10 mx-auto text-light-shades'>
-        <div>
+      <div
+        className='container flex justify-between items-center p-10 mx-auto text-light-shades
+        max-md:flex-col-reverse max-md:gap-8'
+      >
+        <div className='flex flex-col gap-1'>
           <div className='flex items-center gap-4'>
-            <Code size={32} />
+            <Code className='size-10 max-sm:size-8' />
             <span>Made By Lucas Munhoz</span>
           </div>
 
-          <p className='text-gray-400 text-sm font-light'>
+          <p className='text-gray-400 text-sm max-sm:text-xs font-light'>
             Thanks everybody for your attention to my portfolio
           </p>
         </div>
 
-        <span>
+        <span className='max-md:text-sm max-md:text-center'>
           Copyright © 2024{' '}
           <Link className='relative underline-hover text-brand-color' href=''>
             munhoz.tech
-          </Link>.{' '}
-          All rights reserved
+          </Link>.
+          {' '}All rights reserved
         </span>
       </div>
     </footer>
