b660408b0133984595d41d8f35a5f1daa6cacc7d
munhoz2k <lucasmunhozarruda@gmail.com>
2024-04-26 00:06:28 -0300

    style: changing head title

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Create Next App',
+  title: 'Munhoz Portfolio',
 }
 
 export default function LocaleLayout({
